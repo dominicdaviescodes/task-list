e3a0e9233fecfacaba30e3b9f55c8c545424bf54
dominicdaviescodes <codewithdom@gmail.com>
2021-09-10 21:50:41 +0100

    add markup

---
+++ app.js
@@ -0,0 +1,20 @@
+// Define UI Vars
+
+// Load all event listeners
+
+// Load all event listeners
+// Add task event
+
+// Add Task
+
+// Create li element
+// Add class
+// Create text node and append to li
+// Create new link element
+// Add class
+// Add icon html
+// Append the link to li
+
+// Append li to ul
+
+// Clear input
